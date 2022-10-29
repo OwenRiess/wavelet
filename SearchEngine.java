903621904975747c30e01cb03e609823e95ac65d
OwenRiess <owenyriess@gmail.com>
2022-10-28 22:37:07 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -6,28 +6,36 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> words = new ArrayList<String>();
-
+ 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("List of words: %s", words);
         } 
-        else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("p")) {
-                    words.add("pineapple");
-                    return String.format("added %s!", parameters[1]);
-                }
-                else if (parameters[0].equals("a")){
-                    words.add("apple");
-                    return String.format("added %s!", parameters[1]);
-                }
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                words.add(parameters[1]);
+                return String.format("added %s!", parameters[1]);
             }
-            return "404 Not Found!";
         }
+        if (url.getPath().contains("/search")) {
+            ArrayList<String> substring = new ArrayList<String>();
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                for (String word: words) {
+                    if (word.contains(parameters[1])) {
+                        if (!substring.contains(word)) {
+                            substring.add(word);
+                        }
+                    }
+                }
+                return String.format("words that contain substring: %s", substring);
+            }
+        }
+        return "404 Not Found!";
     }
 }
+
 
 class SearchEngine {
     public static void main(String[] args) throws IOException {
